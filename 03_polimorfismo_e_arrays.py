53bcb55c1efab1aa6a46773f57a8a6a486d4a7b4
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-27 20:08:47 -0300

    commit 6

--- 03_polimorfismo_e_arrays.py
+++ 03_polimorfismo_e_arrays.py
@@ -57,31 +57,3 @@ for conta in contas:
 arr.array("d", [1.0, 3.5])
 numeros = np.array([1.0, 3.5])
 print(numeros)
-
-
-class ContaSalario:
-    def __init__(self, codigo):
-        self._codigo = codigo
-        self._saldo = 0
-
-    def deposita(self, valor):
-        self._saldo += valor
-
-    def __eq__(self, outro):
-        if type(outro) != ContaSalario:
-            return False
-        # return self._codigo == outro._codigo
-        return self._codigo == outro._codigo and self._saldo == outro._saldo
-
-    def __str__(self):
-        return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
-
-
-conta1 = ContaSalario(37)
-print(conta1)
-
-conta3 = ContaSalario(37)
-conta4 = ContaSalario(37)
-print(conta3 == conta4)
-
-print(isinstance(ContaCorrente(34), ContaCorrente))
